53a3600df3f9e2c0d089b5643275fe31747c072a
Dmitry Kolesnikov <koldim2001@yandex.ru>
2023-05-08 20:24:43 +0300

    some changes in code were added

--- web.py
+++ web.py
@@ -18,14 +18,17 @@ st.markdown("""
 st.markdown('## Генерирую текстовое описание изображений ')
 st.markdown('made by Kolesnikov Dmitry')
 #st.markdown('---')
+
 image = st.file_uploader("Загрузите изображение", type=['png', 'jpg','jpeg'])
 if image is not None:
     st.image(image)
     img = Image.open(image)
+    
     st.markdown('Результат генерации описания (модель LSTM + ResNet):')
     text = predict(img)
     st.markdown('#### __' + text + '__')
     st.markdown('---')
+
     st.markdown('Результат генерации описания (модель LSTM + MobileNet + Attention):')
     text_att = predict_att(img)
     print(text_att )
